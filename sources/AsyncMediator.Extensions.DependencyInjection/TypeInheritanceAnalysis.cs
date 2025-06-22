59582fba1689155731ae6b1cd67f124ee9206100
Alexandru Iuga <alex.iuga@gmail.com>
2025-06-22 10:01:46 +0300

    update license in code and nuget package.

--- sources/AsyncMediator.Extensions.DependencyInjection/TypeInheritanceAnalysis.cs
+++ sources/AsyncMediator.Extensions.DependencyInjection/TypeInheritanceAnalysis.cs
@@ -1,4 +1,12 @@
-﻿namespace AsyncMediator.Extensions.DependencyInjection;
+﻿// Copyright © 2025 Dust in the Wind 
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+namespace AsyncMediator.Extensions.DependencyInjection;
 
 internal class TypeInheritanceAnalysis
 {
